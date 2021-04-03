40f511856ca3d61db32330b1c6bdd6bbf42ed8f0
Ewan McNeil <54862041+EwanMcNeil@users.noreply.github.com>
2021-04-03 19:11:50 -0400

    Update testBench.sv

--- ProjectTwo/testBench.sv
+++ ProjectTwo/testBench.sv
@@ -1,4 +1,4 @@
-program test(dut_if IF);
+program testBench(dut_IF.TEST IF);
  import classes::*;
  //Generator generator;
  //Agent agent;
@@ -8,27 +8,77 @@ program test(dut_if IF);
  Monitor monitor;
 
 
-    Transaction tarray[10];
-
-
-
+    Transaction tarray[50];
     initial begin
-        
+        driver = new();
+        driver.IF = IF;
+
+        monitor = new();
+        monitor.IF = IF;
+
+        reset();
+        //just to test the driver and the display 
         generator();
-
-        foreach (tarray[i]) begin
-        $display("cmd: %b, data_in: %b, tag_in: %b", tarray[i].cmd,tarray[i].data_in,tarray[i].tag_in);
-
+        for(int i = 0; i < 50; i++)begin
+            tarray[i].displayInputs();
         end
 
-
+        foreach(tarray[i])begin
+            driver.transmitOnePacket(tarray[i]);
+        	@(IF.cb);
+       		@(IF.cb);
+        	@(IF.cb);
+        	@(IF.cb);
+            monitor.displayInterface();
+	@(IF.cb);s
+	 reset();
+            
+        end
     end
+    
 
+    //TODO move into class and move randomization into it
     task generator();
-        foreach (tarray[i]) begin
+        for(int i = 0; i < 50; i++)begin
+            $display("making new object");
             tarray[i] = new();
+	    tarray[i].randomize();
         end
     endtask 
         
+
+    task reset();
+        IF.reset = 1; 
+        IF.cb.req1_cmd_in <= 0000;
+        IF.cb.req2_cmd_in <= 0000;
+        IF.cb.req3_cmd_in <= 0000;
+        IF.cb.req4_cmd_in <= 0000;
+        
+        IF.cb.req1_data_in <= 00000000000000000000000000000000;
+        IF.cb.req2_data_in <= 00000000000000000000000000000000;
+        IF.cb.req3_data_in <= 00000000000000000000000000000000;
+        IF.cb.req4_data_in <= 00000000000000000000000000000000;
+
+        IF.cb.req1_tag_in <= 00;
+        IF.cb.req2_tag_in <= 00;
+        IF.cb.req3_tag_in <= 00;
+        IF.cb.req4_tag_in <= 00;
+
+
+
+
+
+        @(IF.cb);
+        @(IF.cb);
+        @(IF.cb);
+        @(IF.cb);
+        @(IF.cb);
+        @(IF.cb);
+        @(IF.cb);
+        @(IF.cb);
+        @(IF.cb);
+
+        IF.reset = 0; 
+    endtask
 
 endprogram
